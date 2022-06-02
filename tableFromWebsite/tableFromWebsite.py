d9bed991944d314efeafcaaec4d838bb178e7a7f
nicogno <cogno.nicolo@gmail.com>
2022-06-02 14:49:55 +0200

    Added print to CSV file for  tableFromWebsite

--- tableFromWebsite/tableFromWebsite.py
+++ tableFromWebsite/tableFromWebsite.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
 
 url="https://iscrizioni.wedosport.net/lista_iscritti.cfm?code=tre-rifugi-val-pellice-2022"
 page=requests.get(url)
@@ -18,3 +20,16 @@ for row in rows:
     data.append([ele for ele in cols if ele]) # Get rid of empty values
 print(data)
 '''
+now = datetime.now()
+date_string = now.strftime("%d/%m/%Y %H:%M:%S")
+list_data=[str(len(rows)), date_string]
+# open the file in the 'append' mode
+with open('./signed_up.csv', 'a', encoding='UTF8', newline='') as f:
+    # create the csv writer
+    writer = csv.writer(f)
+
+    # write a row to the csv file
+    writer.writerow(list_data)
+
+    # close the file
+    f.close()
